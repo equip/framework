91411fc28c494ad44dd108e34bbde7bd433cfbbc
Adam Meech <adam.meech@wheniwork.com>
2015-05-29 09:41:31 -0500

    Refactor responder tests to be DRY

---
+++ tests/Responder/AbstractResponderTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace SparkTests;
+
+use Aura\Payload\Payload;
+use Spark\Application;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class AbstractResponderTest extends TestCase {
+
+    protected $request, $response;
+
+    public function setUp()
+    {
+        $this->request = Application::boot()->getInjector()->make('Psr\Http\Message\ServerRequestInterface');
+        $this->response = Application::boot()->getInjector()->make('Psr\Http\Message\ResponseInterface');
+    }
+
+    public function statusCodeProvider()
+    {
+        return [
+            ['unknown', 500],
+            [null, 204],
+            [Payload::ACCEPTED, 202],
+            [Payload::CREATED, 201],
+            [Payload::DELETED, 204],
+            [Payload::ERROR, 500],
+            [Payload::FAILURE, 400],
+            [Payload::FOUND, 200],
+            [Payload::NOT_AUTHENTICATED, 400],
+            [Payload::NOT_AUTHORIZED, 403],
+            [Payload::NOT_FOUND, 404],
+            [Payload::NOT_VALID, 422],
+            [Payload::PROCESSING, 203],
+            [Payload::SUCCESS, 200],
+            [Payload::UPDATED, 303],
+        ];
+    }
+
+    /**
+     * @dataProvider statusCodeProvider
+     */
+    public function testStatusCode($status, $expected)
+    {
+        $payload = null;
+        if ($status) {
+            $payload = new Payload;
+            $payload->setStatus($status);
+        }
+
+        $responderClass = $this->getMockForAbstractClass('Spark\Responder\AbstractResponder');
+
+        $responder = new $responderClass;
+        $result = $responder($this->request, $this->response, $payload);
+        $this->assertEquals($expected, $result->getStatusCode());
+    }
+}
