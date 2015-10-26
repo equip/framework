ff1210b6bdb1ed3fac0b2e3311cf5242ff90416c
elazar <tobias382@gmail.com>
2015-10-26 12:35:14 -0500

    Remove response handling in HttpBadRequestException

--- src/Exception/HttpBadRequestException.php
+++ src/Exception/HttpBadRequestException.php
@@ -2,19 +2,11 @@
 namespace Spark\Exception;
 
 use Psr\Http\Message\ResponseInterface;
-use Zend\Diactoros\Stream;
 
 class HttpBadRequestException extends \RuntimeException
 {
     public function getStatusCode()
     {
         return 400;
-    }
-
-    public function withResponse(ResponseInterface $response)
-    {
-        $stream = new Stream('php://memory', 'w');
-        $stream->write(json_encode(['error' => $this->getMessage()]));
-        return $response->withBody($stream);
     }
 }
