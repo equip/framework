842ec6ad791373357d67555ec151379859dbce21
elazar <tobias382@gmail.com>
2015-11-02 08:49:48 -0600

    Fix validation check in MiddlewareCollection

--- src/Middleware/Collection.php
+++ src/Middleware/Collection.php
@@ -23,7 +23,7 @@ class Collection extends \ArrayObject
     protected function validate(array $middlewares)
     {
         foreach ($middlewares as $middleware) {
-            if (!(is_callable($middleware) || $middleware instanceof MiddlewareInterface)) {
+            if (!(is_callable($middleware) || is_subclass_of($middleware, MiddlewareInterface::class))) {
                 throw new \DomainException(
                     'All elements of $middlewares must be callable or implement Relay\\MiddlewareInterface'
                 );
