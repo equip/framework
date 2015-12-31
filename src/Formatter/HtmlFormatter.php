debb7eb7d62b4f8970e6b022129b5c1f1d98f57b
Woody Gilk <woody.gilk@gmail.com>
2015-12-31 14:28:42 -0600

    Clean up formatters
    Normalize whitespace and method precedence in formatters.

--- src/Formatter/HtmlFormatter.php
+++ src/Formatter/HtmlFormatter.php
@@ -6,11 +6,17 @@ use Spark\Adr\PayloadInterface;
 
 abstract class HtmlFormatter extends AbstractFormatter
 {
+    /**
+     * @inheritDoc
+     */
     public static function accepts()
     {
         return ['text/html'];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function type()
     {
         return 'text/html';
