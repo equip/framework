37c10f1aaa0328954011c18cdc1cbd66a4a7d8f2
Alex Masterov <alex.masterow@gmail.com>
2016-05-25 17:55:13 +0300

    Update Payload for changes to interface
    Additional methods for settings were added to the payload interface.
    
    Having all properties that return arrays initialized as arrays prevents
    errors when using return values.

--- tests/PayloadTest.php
+++ tests/PayloadTest.php
@@ -3,28 +3,29 @@
 namespace EquipTests;
 
 use Equip\Payload;
+use PHPUnit_Framework_TestCase as TestCase;
 
-class PayloadTest extends \PHPUnit_Framework_TestCase
+class PayloadTest extends TestCase
 {
     public function testStatus()
     {
         $load = new Payload;
         $copy = $load->withStatus(Payload::STATUS_OK);
 
-        $this->assertNull($load->getStatus());
+        $this->assertEmpty($load->getStatus());
         $this->assertSame(Payload::STATUS_OK, $copy->getStatus());
     }
 
     public function testInput()
     {
         $input = [
-            'test' => true,
+            'test' => true
         ];
 
         $load = new Payload;
         $copy = $load->withInput($input);
 
-        $this->assertNull($load->getInput());
+        $this->assertEmpty($load->getInput());
         $this->assertSame($input, $copy->getInput());
     }
 
@@ -32,26 +33,44 @@ class PayloadTest extends \PHPUnit_Framework_TestCase
     {
         $messages = [
             'username' => 'not found',
-            'password' => 'invalid',
+            'password' => 'invalid'
         ];
 
         $load = new Payload;
         $copy = $load->withMessages($messages);
 
-        $this->assertNull($load->getMessages());
+        $this->assertEmpty($load->getMessages());
         $this->assertSame($messages, $copy->getMessages());
     }
 
     public function testOutput()
     {
         $output = [
-            'collection' => [],
+            'collection' => []
         ];
 
         $load = new Payload;
         $copy = $load->withOutput($output);
 
-        $this->assertNull($load->getOutput());
+        $this->assertEmpty($load->getOutput());
         $this->assertSame($output, $copy->getOutput());
+    }
+
+    public function testSettings()
+    {
+        $name = 'template';
+        $value = 'index';
+
+        $load = new Payload;
+        $copy = $load->withSetting($name, $value);
+
+        $this->assertEmpty($load->getSettings());
+        $this->assertSame($value, $copy->getSetting($name));
+        $this->assertSame([$name => $value], $copy->getSettings());
+
+        $empty = $copy->withoutSetting($name);
+
+        $this->assertNull($empty->getSetting($name));
+        $this->assertEmpty($empty->getSettings());
     }
 }
