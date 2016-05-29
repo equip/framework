e48f1083f34875ff23780ab31679a61764584e4e
Woody Gilk <woody.gilk@gmail.com>
2016-05-29 07:40:54 -0500

    Remove deprecation notes on content handlers
    While the actual content handling comes straight from Relay, we have
    some different requirements, specifically in JSON content handling.
    Rather than trying to remove these, which could make upgrades more
    difficult, remove the parts that are duplication and un-deprecate the
    classes that are compatible. This gives us more flexibility to adjust
    to Relay upgrades.
    
    Also add in a missing HTTP exception test.

--- src/Handler/FormContentHandler.php
+++ src/Handler/FormContentHandler.php
@@ -4,9 +4,6 @@ namespace Equip\Handler;
 
 use Relay\Middleware\FormContentHandler as AbstractHandler;
 
-/**
- * @deprecated 1.4.0 Switched to Relay.Middleware
- */
 class FormContentHandler extends AbstractHandler
 {
 }
--- src/Handler/JsonContentHandler.php
+++ src/Handler/JsonContentHandler.php
@@ -5,9 +5,6 @@ namespace Equip\Handler;
 use Equip\Exception\HttpException;
 use Relay\Middleware\JsonContentHandler as AbstractHandler;
 
-/**
- * @deprecated 1.4.0 Switched to Relay.Middleware
- */
 class JsonContentHandler extends AbstractHandler
 {
     /**
@@ -16,14 +13,6 @@ class JsonContentHandler extends AbstractHandler
     public function __construct($assoc = true, $maxDepth = 512, $options = 0)
     {
         return parent::__construct($assoc, $maxDepth, $options);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    protected function isApplicableMimeType($mime)
-    {
-        return preg_match('~^application/([a-z.]+\+)?json($|;)~', $mime);
     }
 
     /**
