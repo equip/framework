6212d4d0b922c32db4655106e2bc85012b553f64
Woody Gilk <woody.gilk@gmail.com>
2016-05-31 09:53:22 -0500

    Remove deprecation notes on content handlers (#54)
    While the actual content handling comes straight from Relay, we have
    some different requirements, specifically in JSON content handling.
    Rather than trying to remove these, which could make upgrades more
    difficult, remove the parts that are duplication and un-deprecate the
    classes that are compatible. This gives us more flexibility to adjust
    to Relay upgrades.
    
    Also add in a missing HTTP exception test.

--- src/Handler/ContentHandler.php
+++ src/Handler/ContentHandler.php
@@ -4,9 +4,6 @@ namespace Equip\Handler;
 
 use Relay\Middleware\ContentHandler as AbstractHandler;
 
-/**
- * @deprecated 1.4.0 Switched to Relay.Middleware
- */
 abstract class ContentHandler extends AbstractHandler
 {
 }
