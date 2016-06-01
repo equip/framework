19fe98f990367b4b2dc1a979b5527c0b99dbc4d3
Alex Masterov <alex.masterow@gmail.com>
2016-06-01 21:55:22 +0300

    Improve quality to be Scrutinizer CI compliant (#57)
    * Use a single line comment
    
    * Add a separate method to retrieve the Request body

--- src/Configuration/DiactorosConfiguration.php
+++ src/Configuration/DiactorosConfiguration.php
@@ -17,13 +17,9 @@ class DiactorosConfiguration implements ConfigurationInterface
      */
     public function apply(Injector $injector)
     {
+        // Wrong type hint. see https://github.com/relayphp/Relay.Relay/issues/25
         $injector->alias(
             RequestInterface::class,
-            // It should not be necessary to force all requests to be server
-            // requests, except that Relay uses the wrong type hint:
-            // https://github.com/relayphp/Relay.Relay/issues/25
-            //
-            // 'Zend\Diactoros\Request'
             ServerRequest::class
         );
 
