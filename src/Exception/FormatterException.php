ee0ee0434136d28cd0c7fcbe7b5f615e3517f0be
Alex Masterov <alex.masterow@gmail.com>
2016-06-06 16:28:29 +0300

    Structure does not allow storage of objects (#62)

--- src/Exception/FormatterException.php
+++ src/Exception/FormatterException.php
@@ -8,16 +8,12 @@ use InvalidArgumentException;
 class FormatterException extends InvalidArgumentException
 {
     /**
-     * @param string|object $spec
+     * @param string $spec
      *
      * @return static
      */
     public static function invalidClass($spec)
     {
-        if (is_object($spec)) {
-            $spec = get_class($spec);
-        }
-
         return new static(sprintf(
             'Formatter class `%s` must implement `%s`',
             $spec,
@@ -26,16 +22,12 @@ class FormatterException extends InvalidArgumentException
     }
 
     /**
-      * @param string|object $formatter
+      * @param string $formatter
       *
       * @return static
       */
      public static function needsQuality($formatter)
      {
-         if (is_object($formatter)) {
-             $formatter = get_class($formatter);
-         }
-
          return new static(sprintf(
              'No quality have been set for the `%s` formatter',
              $formatter
--- src/Exception/ResponderException.php
+++ src/Exception/ResponderException.php
@@ -8,16 +8,12 @@ use InvalidArgumentException;
 class ResponderException extends InvalidArgumentException
 {
     /**
-     * @param string|object $spec
+     * @param string $spec
      *
      * @return static
      */
     public static function invalidClass($spec)
     {
-        if (is_object($spec)) {
-            $spec = get_class($spec);
-        }
-
         return new static(sprintf(
             'Responder class `%s` must implement `%s`',
             $spec,
